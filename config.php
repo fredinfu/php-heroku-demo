34ede5ff99cf8bffceba7021497cabf710341917
Fredin Funez <developer@Fredins-iMac.local>
2019-07-15 16:04:11 -0600

    test 3

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+    $host = 'us-cdbr-iron-east-02.cleardb.net';
+    $user = 'bef5f1971b3774';
+    $password = '28604589';
+    $db = 'heroku_bf5ec871348ebab';
+    $con = mysqli_connect($host, $user, $password, $db);
